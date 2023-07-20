e37243334ee9275710877a8daefe39e8ea5d76fc
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-07-21 00:57:02 +0300

    Added While

---
+++ While.py
@@ -0,0 +1,19 @@
+i = 9
+while i > 0:
+    print(i, end = "-")
+    i -=1
+
+for i in (5, 7, "fofmfo", True, [1,2]):
+    print(i)
+
+for _ in range(4):
+    print("Hello")
+
+for i in [5, 8, 9, 77]:
+    print(i)
+
+
+sp = []
+sp.append(88)
+sp.append("Hello")
+print(sp)
