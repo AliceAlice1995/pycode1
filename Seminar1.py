aeff3e9fc727783b197ba14783b8ad330b416ed9
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-07-21 00:54:48 +0300

    Added .folder

--- Seminar1.py
+++ Seminar1.py
@@ -1,6 +1,14 @@
 # speed = int(input("Введите скорость автомобиля: "))
 # time = float(input("Введите время поездки: "))
 
-speed = 110
-time = 3.5
-print("Вы проехали", speed / time)
+# # speed = 110
+# # time = 3.5
+# print("Вы проехали: ", time * speed)
+
+num = int(input("Введите число: "))
+if num > 0: 
+    print("Это число положительное")
+elif num == 0:
+    print("Это ноль")
+else:
+    print("Это число отрицательное")
