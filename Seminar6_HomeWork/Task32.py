9d020f27e26c6f211bc6223cbf0b64c21bd0fd49
Alisa Lubinets <lubinets.alisa@gmail.com>
2023-08-15 09:59:23 +0300

    Added Seminar6_HomeWork

---
+++ Seminar6_HomeWork/Task32.py
@@ -0,0 +1,24 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше 
+# заданного максимума). Список можно задавать рандомно
+# На входе : [ 1, 5, 88, 100, 2, -4]
+# 33
+# 200
+# Ответ: [2, 3]
+
+import random
+
+min = int(input('Мин-ое значение: '))
+max = int(input('Макс-ое значение: '))
+
+rand_list = [random.randint(-100,100) for _ in range(6)]
+print(rand_list)
+
+indexes = []
+
+for i in range(0, len(rand_list)):
+    if rand_list[i] >= min and rand_list[i] <= max:
+        indexes.append(i)
+
+print(indexes)
